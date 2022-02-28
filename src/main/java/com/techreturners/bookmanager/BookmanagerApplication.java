1ca483e5b1dd021cff4a363a62b343c907519194
lauraPrp <laura.porpiglia@gmail.com>
2022-02-28 14:42:42 +0000

    using the Builder pattern to construct the Docket and  ApiInfo instances.

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -2,12 +2,37 @@ package com.techreturners.bookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagerApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.withClassAnnotation (RestController.class))
+				.build()
+				.apiInfo(apiInformation());
+	}
+	private ApiInfo apiInformation() {
+		return new ApiInfoBuilder()
+				.title(" Minimalist Book Manager API")
+				.description("Rumors say the last two novels of \"Game of Thrones\" by George Martin are accessible in this API...  ")
+				.version("v1")
+				.build();
 	}
 
 }
